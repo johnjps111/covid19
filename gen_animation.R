3da0771df9d14b0aabcda020e8b21039b2b1c6de
John Schneider <johnjps111@gmail.com>
2020-08-08 20:20:41 -0500

    Update gen_animation.R
    increased frame per second

--- gen_animation.R
+++ gen_animation.R
@@ -1,23 +1,24 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+filePath = dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(filePath)
 
 library(magick)
 
 # cases_file_list <- list.files(path = "./plots/cases/", pattern = "*.jpg", full.names = T)
 # mcimg <- image_read(cases_file_list)
-# mcimg <- image_animate(mcimg, fps = 5, loop = 1, optimize = TRUE)
+# mcimg <- image_animate(mcimg, fps = 10, loop = 1, optimize = TRUE)
 # image_write(mcimg, "anim_cases.gif")
 
 # deaths_file_list <- list.files(path = "./plots/deaths/", pattern = "*.jpg", full.names = T)
 # mdimg <- image_read(deaths_file_list)
-# mdimg <- image_animate(mdimg, fps = 5, loop = 1, optimize = TRUE)
+# mdimg <- image_animate(mdimg, fps = 10, loop = 1, optimize = TRUE)
 # image_write(mdimg, "anim_deaths.gif")
 
-# cases_file_list <- list.files(path = "./plots/case_pcts/", pattern = "*.jpg", full.names = T)
-# mcimg <- image_read(cases_file_list)
-# mcimg <- image_animate(mcimg, fps = 5, loop = 1, optimize = TRUE)
+# case_pcts_file_list <- list.files(path = "./plots/case_pcts/", pattern = "*.jpg", full.names = T)
+# mcimg <- image_read(case_pcts_file_list)
+# mcimg <- image_animate(mcimg, fps = 10, loop = 1, optimize = TRUE)
 # image_write(mcimg, "anim_case_pcts.gif")
 
-deaths_file_list <- list.files(path = "./plots/death_pcts/", pattern = "*.jpg", full.names = T)
-mdimg <- image_read(deaths_file_list)
-mdimg <- image_animate(mdimg, fps = 5, loop = 1, optimize = TRUE)
+death_pcts_file_list <- list.files(path = "./plots/death_pcts/", pattern = "*.jpg", full.names = T)
+mdimg <- image_read(death_pcts_file_list)
+mdimg <- image_animate(mdimg, fps = 10, loop = 1, optimize = TRUE)
 image_write(mdimg, "anim_death_pcts.gif")
